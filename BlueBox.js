228c74bda2799bca7e4396dfc2f55421458e75fb
SRILOAKH <76691584+SRILOAKH@users.noreply.github.com>
2021-01-26 20:03:21 +0530

    Add files via upload

---
+++ BlueBox.js
@@ -0,0 +1,27 @@
+class BlueBox extends Box{
+    constructor(x, y){
+        super(x,y,20,30);
+        this.Visibility = 255;
+      }
+    display(){
+    console.log(this.body.speed);
+    
+    if(this.body.speed < 3){
+      super.display();
+    
+    }
+    else{
+      //do nothing
+      World.remove(world,this.body);
+      push();
+    this.Visibility=this.Visibility - 5;
+    tint(255,this.Visibility);
+      image(this.image,this.body.position.x,this.body.position.y,50,50);
+      pop();
+    }
+    
+    
+    
+    
+    }
+    };
